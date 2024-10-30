226f73f377230735fc03da3c9863325a6b636930
Alexavier Brink <65963015+Tubbster-UX@users.noreply.github.com>
2024-10-29 20:50:23 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div class="flex flex-row min-h-screen justify-center items-center bg-black">
+      <div className="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 flex flex-col justify-center text-center space-y-10 sm:space-y-20 md:space-y-30">
+        <img src='Limitless Auto.png' className="w-3/4 mx-auto" />
+        <h1 className="text-xl sm:text-2xl md:text-2xl lg:text-4xl font-black text-white">COMING SOON</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
